308cc847c0bf50a2bcd34357033d9b5acd525b3a
PYJ <yeonji.park.kr@gmail.com>
2026-06-08 13:13:26 +0900

    feat: auth api 추가 (step 4까지) - jwt, security 의존성 및 filter chain 추가 - 로그인 api - DataInitializer 추가 - front 연동 cors 설정

--- src/main/java/com/sesac/aibackend/error/GlobalExceptionHandler.java
+++ src/main/java/com/sesac/aibackend/error/GlobalExceptionHandler.java
@@ -5,10 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// 이쪽에서 custom exception 을 등록 해줘야 handler 가 그 에러를 catch 해서 custom logic에 따라 처리함
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of("NOT_FOUND",e.getMessage()));
+    }
+
+    // DuplicationException throw 되면 이 hadler 에서 catch 해서 409 CONFLICT 로 응답
+    @ExceptionHandler(DuplicateException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicate(DuplicateException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(ErrorResponse.of("CONFLICT", e.getMessage()));
     }
 }
